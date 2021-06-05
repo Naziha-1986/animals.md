daaa522c80b72389d7c9a558d8974ed4e82751e3
Naziha86 <>
2021-06-05 15:47:54 +0000

    added new repo. and html,css and js files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# animals.md
+# Introdution: 
+ _**Hi every one **_
+
+My name is **Naziha Qawasmeh**.iam from Joradn. Iam 34 years old.I graduated from Yarmouk Universiy. 
+
+By the way, Iam an *Accountant*.I obtained Master degree in Accounting Major in 2020.
+
+I joined to your team to become a web developer and to have the ability to design web pages that will help me in my work- when i get one- and improve accounting field in general.
+
+_**Iam so exciting for this journey, i feel it will be fun and great one.**_
+
+### *nice to meet you all*.
+
+
+
+## Table of contents:
+
+|  content       | link |
+| ----------    | ----------- |
+| Read: 01       | [link](https://replit.com/@Naziha86/reading-notesmd#Read:01.md) |
+| lab:01         |[link](https://replit.com/@Naziha86/reading-notesmd#Lab:01b.md)        |
+|Read:02|[link](https://replit.com/@Naziha86/reading-notesmd#Read:02.md)
+|Read:03 | [link](https://replit.com/@Naziha86/reading-notesmd#Read:03.md)|
+|Read:04| [link](https://replit.com/@Naziha86/reading-notesmd#Read:04.md)|
+|Read:06|[link](https://replit.com/@Naziha86/reading-notesmd#Read:06.md)
+|Read:07|[lin
---
+++ script.js
@@ -0,0 +1,51 @@
+alert("welcome every one,nice to meet you")
+
+var userName;
+userName = "Naziha Qawasmeh";
+
+console.log(userName);
+
+document.write("<p style='color:brown;'> this is " + userName + " from JS </p>")
+var x = "10";
+var y = 20;
+
+console.log(y == x)
+//y and x are not equal in value (20>10)so the result will be false
+console.log(y === x);
+//y and xare not equal in value and type so the result will be false
+console.log(y == 10);
+// y is not = 10 
+
+console.log(y == 20);
+// y is = 20 in value
+
+console.log(y !== x);
+// show the opposite answer
+
+var userluckynumber =prompt("enter your lucky number?","14")
+// i wrote "14" to display in the msg as a default number
+;
+if (userluckynumber == 14) {
+  alert("meeee too")
+} else {
+  alert("that's a good one")
+}
+
+var useryear = prompt("enter your year of birth?");
+
+if (useryear == 1986) {
+  alert("just like me")
+} else if(useryear < 1986) {
+  alert("welcome")
+} else{
+  alert("hi")
+}
+
+
+  var pressabutton;
+var r = confirm("Press a button!");
+if (r == true) {
+  pressabutton = "You pressed OK!";
+} else {
+  pressabutton = "You pressed Cancel!";
+}
